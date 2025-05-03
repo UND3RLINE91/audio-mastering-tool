38c3d0fc5b817838a931c102209ff8a449350730
UND3RLINE91 <corybedwell.ca@gmail.com>
2025-05-03 10:39:44 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -12,15 +12,7 @@ export default defineConfig({
   },
   build: {
     outDir: 'dist',
-    sourcemap: true,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          'react-vendor': ['react', 'react-dom', 'react-router-dom'],
-          'ui-vendor': ['@radix-ui/react-alert-dialog', '@radix-ui/react-avatar', '@radix-ui/react-dialog', '@radix-ui/react-dropdown-menu', '@radix-ui/react-label', '@radix-ui/react-progress', '@radix-ui/react-select', '@radix-ui/react-slot', '@radix-ui/react-tabs', '@radix-ui/react-toast', 'lucide-react'],
-        },
-      },
-    },
+    sourcemap: true
   },
   base: '/',
 });
